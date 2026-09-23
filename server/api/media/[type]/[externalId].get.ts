c5155b2f0a67bd0c580457e63f0f4b0bd5c70e87
Derzsi Dezső <derzsidezso@gmail.com>
2026-09-24 00:04:14 +0300

    feat: enrich media responses with bookmark state

--- server/api/media/[type]/[externalId].get.ts
+++ server/api/media/[type]/[externalId].get.ts
@@ -2,12 +2,19 @@ import { defineEventHandler, getRouterParam } from 'h3'
 import { createMediaService } from '../../../services/media'
 import { withRouteErrors } from '../../../utils/route-errors'
 import { parseRuntimeConfig } from '../../../utils/runtime-config'
+import { getOptionalUser } from '../../../utils/auth-context'
 
 export default defineEventHandler((event) =>
-  withRouteErrors(event, () =>
-    createMediaService(parseRuntimeConfig(useRuntimeConfig(event))).details({
-      type: getRouterParam(event, 'type'),
-      externalId: getRouterParam(event, 'externalId')
-    })
-  )
+  withRouteErrors(event, async () => {
+    const user = await getOptionalUser(event)
+    return createMediaService(
+      parseRuntimeConfig(useRuntimeConfig(event))
+    ).details(
+      {
+        type: getRouterParam(event, 'type'),
+        externalId: getRouterParam(event, 'externalId')
+      },
+      user?.id
+    )
+  })
 )
--- server/api/media/trending.get.ts
+++ server/api/media/trending.get.ts
@@ -2,11 +2,13 @@ import { defineEventHandler, getQuery } from 'h3'
 import { createMediaService } from '../../services/media'
 import { withRouteErrors } from '../../utils/route-errors'
 import { parseRuntimeConfig } from '../../utils/runtime-config'
+import { getOptionalUser } from '../../utils/auth-context'
 
 export default defineEventHandler((event) =>
-  withRouteErrors(event, () =>
-    createMediaService(parseRuntimeConfig(useRuntimeConfig(event))).trending(
-      getQuery(event)
-    )
-  )
+  withRouteErrors(event, async () => {
+    const user = await getOptionalUser(event)
+    return createMediaService(
+      parseRuntimeConfig(useRuntimeConfig(event))
+    ).trending(getQuery(event), user?.id)
+  })
 )
